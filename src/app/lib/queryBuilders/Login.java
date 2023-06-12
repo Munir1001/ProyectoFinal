7ab32f62471fe653f8e9724106b912a6b1438d19
GabrielPico24 <133067000+GabrielPico24@users.noreply.github.com>
2023-06-12 01:34:27 -0500

    Login

---
+++ src/app/lib/queryBuilders/Login.java
@@ -0,0 +1,18 @@
+package app.lib.queryBuilders;
+
+public class Login implements QueryBuilder {
+	private final static String template = "CREATE LOGIN %s WITH PASSWORD = '%s';";
+	private String username;
+	private String password;
+
+	public Login(String username,String password) {
+		this.username = username;
+		this.password = password;
+	}
+
+	@Override
+	public String generateQuery(Object... params) {
+		return String.format(this.template, this.username, this.password);
+	}
+
+}
