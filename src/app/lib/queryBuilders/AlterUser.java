d92874755f5a53c5e2159c9bb9bac2f8f11fbc1e
GabrielPico24 <133067000+GabrielPico24@users.noreply.github.com>
2023-06-12 01:38:46 -0500

    aluser

---
+++ src/app/lib/queryBuilders/AlterUser.java
@@ -0,0 +1,25 @@
+package app.lib.queryBuilders;
+
+public class AlterUser implements QueryBuilder {
+	private final static String template = "ALTER LOGIN %s WITH %s = '%s';";
+	private Fields field;
+	private String login;
+	private String newValue;
+
+	public AlterUser(String login, Fields field, String newValue) {
+		this.login = login;
+		this.field = field;
+		this.newValue = newValue;
+	}
+
+	@Override
+	public String generateQuery(Object... params) {
+		System.out.println(String.format(template,this.login, this.field.toString(), this.newValue));
+		return String.format(template,this.login, this.field.toString(), this.newValue);
+	}
+
+	public enum Fields {
+		NAME,PASSWORD
+	}
+
+}
