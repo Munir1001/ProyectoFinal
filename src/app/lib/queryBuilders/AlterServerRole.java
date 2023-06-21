fc7fbc2effa923f9dc56bca7dfcc5e820d5bad75
GabrielPico24 <133067000+GabrielPico24@users.noreply.github.com>
2023-06-21 07:30:02 -0500

    Creación AlterServerRole

---
+++ src/app/lib/queryBuilders/AlterServerRole.java
@@ -0,0 +1,18 @@
+package app.lib.queryBuilders;
+
+public class AlterServerRole implements QueryBuilder {
+	private final String template = "ALTER SERVER ROLE %s ADD MEMBER %s;";
+	private String user;
+	private String role;
+
+	public AlterServerRole(String role,String user) {
+		this.role = role;
+		this.user = user;
+	}
+
+	@Override
+	public String generateQuery(Object... params) {
+		return String.format(this.template, this.role,this.user);
+	}
+
+}
